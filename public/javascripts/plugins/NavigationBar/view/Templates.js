9b871168291ccc24c5ee9193a4f85a1243841a69
Josh Bass <jbass86@gmail.com>
2013-12-04 23:36:19 -0500

    fixed up some gui things for the customer section

--- public/javascripts/plugins/NavigationBar/view/Templates.js
+++ public/javascripts/plugins/NavigationBar/view/Templates.js
@@ -2,5 +2,14 @@
 		define(["vendor/handlebars"], function(Handlebars) {
 			return {
 		
-"NavigationBar": Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {  this.compilerInfo = [2,'>= 1.0.0-rc.3'];helpers = helpers || Handlebars.helpers; data = data || {};    return "<div class=\"navigationBarTitle\">	<span class=\"navBarLabel\">Navigation Bar</span>	<div class=\"expandButton\">		<span class=\"ui-icon ui-icon-arrow-4-diag\"></span>	</div></div><div class=\"section\">	<span>Customer Section</span></div><div class=\"section\">	<span>Inventory Section</span></div><div class=\"section\">	<span>Order Section</span></div>";  })
+"NavigationBar": Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Handlebars.helpers; data = data || {};
+  
+
+
+  return "<div class=\"navigationBarTitle\">\r\n	<span class=\"navBarLabel\">Navigation Bar</span>\r\n	<div class=\"expandButton\">\r\n		<span class=\"ui-icon ui-icon-arrow-4-diag\"></span>\r\n	</div>\r\n</div>\r\n\r\n<div class=\"viewSelections\">\r\n\r\n	<ul>\r\n		\r\n\r\n		<div class=\"section\">\r\n			<li>\r\n				<span>Customer Section</span>\r\n			</li>\r\n		</div>\r\n\r\n\r\n\r\n		<div class=\"section\">\r\n			<li>\r\n				<span>Inventory Section</span>\r\n			</li>\r\n		</div>\r\n\r\n\r\n\r\n		<div class=\"section\">\r\n			<li>\r\n				<span>Order Section</span>\r\n			</li>\r\n		</div>\r\n		\r\n	<ul>\r\n\r\n</div>\r\n\r\n\r\n\r\n";
+  }
+
+)
 }});
